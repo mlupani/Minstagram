3df89b7d3f7378623cf86c5ee2e99ad5b25d4dd2
miguel <mlupani2@gmail.com>
2021-06-24 11:06:01 -0300

    cambio

--- services/notifications.js
+++ services/notifications.js
@@ -34,6 +34,8 @@ export const sendNotification = async (payload, subscription) => {
 
     const body = {subscription , payload}
 
+    console.log(payload)
+
     try {
 
         await fetch(window.location.origin+'/api/notifications', {
