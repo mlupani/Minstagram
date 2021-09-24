55805910449de483e5041cbda50b97618e4844bc
Miguel Angel Lupani <mlupani2@gmail.com>
2021-09-24 12:32:15 -0300

    chequeo de pushnotifications

--- services/notifications.js
+++ services/notifications.js
@@ -19,7 +19,7 @@ export const subscribeNotifications = async (callback) => {
 
     const PUBLIC_VAPID_KEY = 'BJvVreswBv89i4HbjRtetboHnxf5satBDgWrNBxdEmFv0KuKeODvCRj2wZMda2O5Jhr1YfCp9pf5M-Cjfv367U8'
 
-    navigator.serviceWorker.ready.then(registration => {
+    navigator.serviceWorker.ready.then(async registration => {
         subscription  = await registration?.pushManager.subscribe({
             userVisibleOnly: true,
             applicationServerKey: urlBase64ToUint8Array(PUBLIC_VAPID_KEY)
